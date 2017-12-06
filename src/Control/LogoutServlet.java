84b44c33ffd750817b0b2c1295644c31494e5ecd
gg <metawee.a245@gmail.com>
2017-12-06 17:59:01 +0700

    add logout servlet

--- src/Control/LogoutServlet.java
+++ src/Control/LogoutServlet.java
@@ -1,11 +1,17 @@
 package Control;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import Bean.userBean;
+import Bean.StudentBean;
 
 /**
  * Servlet implementation class LogoutServlet
@@ -27,7 +33,20 @@ public class LogoutServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		HttpSession session = request.getSession();
+		userBean userBean = (userBean)session.getAttribute("currentUser");
+		
+		if(userBean != null){
+			session.invalidate();
+		}
+		
+		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/index.html");
+
+		if (dispatcher != null) {
+
+			dispatcher.forward(request, response);
+
+		}
 	}
 
 	/**
--- src/Control/StdServlet.java
+++ src/Control/StdServlet.java
@@ -8,6 +8,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import Bean.userBean;
 
 /**
  * Servlet implementation class StdServlet
@@ -29,6 +32,9 @@ public class StdServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
+		userBean userBean = (userBean)session.getAttribute("currentUser");
+		
 		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/studentHome.jsp");
 		if (dispatcher != null) {
 			dispatcher.forward(request, response);
